47325563635bab4d3e4c734733a019095377f36f
Marcin Jablonowski <marcin.jab@wp.pl>
2018-03-01 08:14:32 +0100

    StudentAdmin

--- studmanage/smapp/admin.py
+++ studmanage/smapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Student
 
 # Register your models here.
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'school_class', 'year_of_birth', 'suspended']
+    exclude = ('year_of_birth',)  # nie mozna edytowac i go nie widzimy aby byla krotka musi po str byc ,
+
+
+admin.site.register(Student, StudentAdmin)
